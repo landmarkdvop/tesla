0b5838494b1cc649d73660c4e39b043d0c38df5c
raphealatud <raphealatud1998@yahoo.com>
2022-05-19 17:49:30 +0000

    myprayer.sh

---
+++ myprayer.sh
@@ -0,0 +1,10 @@
+salary of $80 an hour and above
+making more than half a million in a year
+getting into the millionaire group as soon as possible
+opening orphanages all over the world is my gaol
+giving to the poor and needy is my priority
+God above everything
+i need the strength and the wisdom for this, please.
+can make it on my own
+thanks.
+
